1d139e6e26727b340eb3d72a400d725dba50f5b4
ssanchez <dev@spencersanchez.com>
2015-04-01 22:15:55 -0400

    replace Caskfile with brew-cask.sh

---
+++ brew-cask.sh
@@ -0,0 +1,46 @@
+# Install native apps
+
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+# daily
+brew cask install alfred
+brew cask install dropbox
+brew cask install gyazo
+brew cask install nvalt
+# brew cask install onepassword
+# brew cask install rescuetime
+brew cask install sizeup
+# cask install flux
+
+# dev
+brew cask install cyberduck
+# brew cask install filezilla
+brew cask install iterm2
+brew cask install imagealpha
+brew cask install imageoptim
+brew cask install sourcetree
+brew cask install sublime-text3
+# brew cask install virtualbox
+
+# fun
+# brew cask install limechat
+brew cask install miro-video-converter
+# brew cask install spotify
+
+# browsers
+brew cask install firefox
+brew cask install google-chrome
+brew cask install google-chrome-canary
+# brew cask install firefox-nightly --force
+# brew cask install webkit-nightly --force
+# brew cask install chromium --force
+# brew cask install torbrowser
+
+# less often
+brew cask install disk-inventory-x
+brew cask install the-unarchiver
+# brew cask install transmission
+brew cask install vlc
+
+
